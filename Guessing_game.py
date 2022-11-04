18e44590fcf802fec1a24837f2a7fe4c19dfa5a8
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-11-04 00:34:45 -0700

    guessinggamae

--- Guessing_game.py
+++ Guessing_game.py
@@ -7,28 +7,32 @@ def method_name():
         print(f"Guess the number!\n ")
         print()
 
-def play_game(LIMIT):
-    number = random.randint(1, LIMIT)
-    print(f'Im thinking of a number from 1 to {LIMIT}\n')
-    count = 1
-    guess = int(input('Your guess: '))
-
-    while (guess != number):
-        if guess < number:
-            print('Too low. ')
-            count += 1
-        elif guess > number:
-            print('Too high. ')
-            count += 1
+    def play_game(LIMIT):
+        number = random.randint(1, LIMIT)
+        print(f'Im thinking of a number from 1 to {LIMIT}\n')
+        count = 1
         guess = int(input('Your guess: '))
-    print(f'You guessed it in {count} tries.\n ')
-
-def main():
-    display_title
-    again = 'y'
-    while again.lower() == 'y':
-        LIMIT = int(input('Enter the limit: '))
-        play_game(LIMIT)
-        again = input('Would you like to play again? (y/n): ')
-        print()
-    print('BYE! ')
+    
+        while (guess != number):
+            if guess < number:
+                print('Too low. ')
+                count += 1
+            elif guess > number:
+                print('Too high. ')
+                count += 1
+            guess = int(input("Your guess: "))
+        print(f'You guessed it in {count} tries.\n ')
+    
+    def main():
+        display_title
+        again = 'y'
+        while again.lower() == 'y':
+            LIMIT = int(input('Enter the limit: '))
+            play_game(LIMIT)
+            again = input('Would you like to play again? (y/n): ')
+            print()
+        print('BYE! ')
+    
+    if __name__ == '__main__':
+        main()
+    return display_title, main, play_game, random
